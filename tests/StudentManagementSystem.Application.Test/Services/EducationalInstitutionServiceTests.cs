d169b50de5d7f40740854890ccdee40cd8784ef0
yurii.khmelenko <yukhme@taltech.ee>
2022-10-23 18:44:28 +0300

    [Use Case 11] failing tests

--- tests/StudentManagementSystem.Application.Test/Services/EducationalInstitutionServiceTests.cs
+++ tests/StudentManagementSystem.Application.Test/Services/EducationalInstitutionServiceTests.cs
@@ -26,15 +26,42 @@ public class EducationalInstitutionServiceTests
         var coursesStub = InstitutionStub.GetCourses();
         A.CallTo(() => _institutionRepository.FindAllCourses(InstitutionId)).Returns(coursesStub);
         var availableCourses = _institutionService.ListAvailableCourses(InstitutionId);
-        
+
         Assert.Equal(coursesStub.Count, availableCourses.Count);
     }
-    
+
     [Fact]
     public void ShouldThrowIfNoCoursesAvailable()
     {
-        A.CallTo(() => _institutionRepository.FindAllCourses(InstitutionId)).Returns(Enumerable.Empty<CourseEntity>().ToList());
+        A.CallTo(() => _institutionRepository.FindAllCourses(InstitutionId))
+            .Returns(Enumerable.Empty<CourseEntity>().ToList());
 
         Assert.Throws<ArgumentException>(() => _institutionService.ListAvailableCourses(InstitutionId));
+    }
+
+    [Fact]
+    public void ShouldFindCourse()
+    {
+        var institutionEntity = InstitutionStub.Get("test", verified: true, removed: false);
+        var expectedCourse = institutionEntity.Courses[0];
+        A.CallTo(() => _institutionRepository.FindCourse(institutionEntity.Id, expectedCourse.Id)).Returns(expectedCourse);
+        var course = _institutionService.FindCourse(institutionEntity.Id, expectedCourse.Id)!;
+
+        Assert.Multiple(
+            () => Assert.Equal(expectedCourse.Name, course.Name),
+            () => Assert.Equal(expectedCourse.StartTime, course.StartTime),
+            () => Assert.Equal(expectedCourse.StudentsCapacity, course.StudentsCapacity),
+            () => Assert.Equal(expectedCourse.MinScoreRequired, course.MinScoreRequired)
+        );
+    }
+    
+    [Fact]
+    public void ShouldThrowIfCourseNotFound()
+    {
+        var institutionEntity = InstitutionStub.Get("test", verified: true, removed: false);
+        var expectedCourse = institutionEntity.Courses[0];
+        A.CallTo(() => _institutionRepository.FindCourse(institutionEntity.Id, expectedCourse.Id)).Returns(null);
+        
+        Assert.Throws<ArgumentException>(() => _institutionService.FindCourse(institutionEntity.Id, expectedCourse.Id));
     }
 }
